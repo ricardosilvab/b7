6677f56d8bf9a8a6c6a2ac95f1649cdbd3078422
Ricardo Barbosa <ricardos-barbosa@hotmail.com>
2024-03-13 23:02:21 -0300

    Mobile responsive

---
+++ projeto-awax/js/script.js
@@ -0,0 +1,10 @@
+// menu buger
+window.onload = function(){
+    document.querySelector(".menu-opener").addEventListener("click", function(){
+        if(document.querySelector("nav").style.display == 'flex') {
+            document.querySelector("nav").style.display = 'none';
+        } else {
+            document.querySelector("nav").style.display = 'flex';
+        }
+    });
+};
